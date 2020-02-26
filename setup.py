71d10d6932367234ec6f2955c4d1ff9acecfc337
HiFiBerry <info@hifiberry.com>
2020-02-26 17:08:58 +0100

    Added setuptools

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+import stats
+
+setup(name='usagecollector',
+      version=stats.__version__,
+      description='Usage data collector',
+      long_description='A collection of tools to collect and read usage data', 
+      url='http://github.com/hifiberry/usagecollector',
+      author='Daniel Matuschek',
+      author_email='daniel@hifiberry.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5'
+      ],
+      packages=find_packages(),
+      install_requires=['bottle'],
+      scripts=[],
+      keywords='statistics, database',
+      zip_safe=False)
